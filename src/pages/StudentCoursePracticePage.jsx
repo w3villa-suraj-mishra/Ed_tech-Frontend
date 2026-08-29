ae498b6eabba83c9bfbd4c329fd38799d3d6b092
w3villa-suraj-mishra <suraj.mishra@w3villa.com>
2026-08-29 12:16:31 +0530

    Add Footer component to StudentCoursePracticePage.jsx

--- src/pages/StudentCoursePracticePage.jsx
+++ src/pages/StudentCoursePracticePage.jsx
@@ -16,6 +16,8 @@ import {
   FiRefreshCw
 } from 'react-icons/fi';
 
+import Footer from '../components/Common/Footer.jsx';
+
 const CATEGORIES = [
   'All',
   'MCQ',
@@ -303,6 +305,7 @@ export default function StudentCoursePracticePage() {
         )}
 
       </div>
+      <Footer />
     </div>
   );
 }
